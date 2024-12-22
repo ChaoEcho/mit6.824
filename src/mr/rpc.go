2ebb27f3361ed2d0de4eab88043c992227d1939d
echochao <echochao@126.com>
2024-12-22 14:02:03 +0800

    修改RPC名称

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -24,13 +24,19 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
-
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
 // Can't use the current directory since
 // Athena AFS doesn't support UNIX-domain sockets.
 func coordinatorSock() string {
 	s := "/var/tmp/5840-mr-"
+	s += strconv.Itoa(os.Getuid())
+	return s
+}
+
+// 先创建另外一个
+func myCoordinatorSock() string {
+	s := "/var/tmp/my-5840-mr-"
 	s += strconv.Itoa(os.Getuid())
 	return s
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -74,7 +74,8 @@ func CallExample() {
 //
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
-	sockname := coordinatorSock()
+	// sockname := coordinatorSock()
+	sockname := myCoordinatorSock()
 	c, err := rpc.DialHTTP("unix", sockname)
 	if err != nil {
 		log.Fatal("dialing:", err)
